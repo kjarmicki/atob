e775dcebb91640ded067d71935fee74c7c49383a
kjarmicki <jarmicki@gmail.com>
2016-07-11 22:23:24 +0200

    disable form autocompletion

--- src/views/points/point-form.jsx
+++ src/views/points/point-form.jsx
@@ -115,9 +115,12 @@ export default class PointForm extends React.Component {
             <div className={pointFormWrapperClassNames}>
                 <button className="btn point-form-trigger" onClick={this.showForm.bind(this)}>Add a new point at current location</button>
                 <div className="point-form-overlay">
-                    <form className={pointFormClassNames} onSubmit={this.submitPoint.bind(this)}>
+                    <form autoComplete="off" className={pointFormClassNames} onSubmit={this.submitPoint.bind(this)}>
                         <div className="point-form-inputs">
-                            <input ref={input => input && (this.input = input)} type="text" onChange={this.updateName.bind(this)} value={this.state.name} name="name" className="new-point-name" placeholder="enter a point name" />
+                            <input ref={input => input && (this.input = input)} type="text"
+                                   onChange={this.updateName.bind(this)} value={this.state.name}
+                                   name="name" className="new-point-name" placeholder="enter a point name"
+                                />
                             <input className="btn" type="submit" value="save" />
                             <button className="btn point-form-cancel" onClick={this.hideForm.bind(this)}>cancel</button>
                         </div>
